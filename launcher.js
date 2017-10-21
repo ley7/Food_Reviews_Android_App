798ff7b26ed131de24f54d2f4d11183acd5027a9
Le Yu <ley7@uci.edu>
2017-10-21 16:48:53 -0700

    food reviews Android App

---
+++ launcher.js
@@ -0,0 +1,9 @@
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+} from 'react-native';
+
+import rootApp from './app/root';
+
+AppRegistry.registerComponent('Elm', () => rootApp);
